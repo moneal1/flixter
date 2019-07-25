f6f2da3a2076c5f54a86f6ff87c64e6ded061183
Mike O'Neal <MikeONeal18@gmail.com>
2019-07-25 23:05:23 +0000

    Course Validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :cost, presence: true, numericality: {greater_than_or_equla_to: 0}
 end
