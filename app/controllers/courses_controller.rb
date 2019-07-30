c7d554dfe89c5bd0bb3ef021c35220771dd9b2d9
Mike O'Neal <MikeONeal18@gmail.com>
2019-07-30 01:25:16 +0000

    Flixter errors

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,15 +2,11 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
-  def each
-    @each = each.new
-  end
+  
 
   def show
     @course = Course.find(params[:id])
   end
-  def title
-    @title = title.new
-  end
+  
   end
 
