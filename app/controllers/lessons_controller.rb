22dd334ecb7ec517c2236b87c7f5d7c56561a8fe
Mike O'Neal <MikeONeal18@gmail.com>
2019-08-13 04:35:07 +0000

    errors on lesson 29

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,20 +7,11 @@ class LessonsController < ApplicationController
 
   private
 
-  def lesson
-    if !current_user.enrolled_in?
-      (current_lesson.section.course)
-
-  
-  end
-
-  def course
-    @course = Courses.new
-  end
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      redirect_to user_session_path(current_lesson.course), alert: 'You Are Not Enrolled', status: :unauthorized
+    if !current_user.enrolled_in?(current_lesson.section.course), alert: 'You Are Not Enrolled', status: :unauthorized
+
     end
 
   helper_method :current_lesson
@@ -28,13 +19,8 @@ class LessonsController < ApplicationController
     @current_lesson ||= Lesson.find(params[:id])
   end
 
-  helper_method :current_section
-def current_section
-  @current_section ||= Section.find(params[:section_id])
 end
-end
-end
-end
+
 
 
 
